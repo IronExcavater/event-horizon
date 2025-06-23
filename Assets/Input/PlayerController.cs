d69b02c0a53e4ce1cff04353a62c032797c542a6
Mellatrix <89572289+Mellatrix@users.noreply.github.com>
2025-06-23 21:01:16 +1000

    Boosting on left mouse click

--- Assets/Input/PlayerController.cs
+++ Assets/Input/PlayerController.cs
@@ -8,18 +8,22 @@ public class PlayerController : MonoBehaviour
           moveSpeed = 10f;
 
     Camera mainCam;
+    Rigidbody2D rb;
+
     bool isBoosting = false;
 
     private void Awake()
     {
         mainCam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
     }
+
     private void Update()
     {
         RotateSelf();
         MoveTowardsTarget();
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetMouseButton(0))
         {
             isBoosting = true;
         }
@@ -30,6 +34,8 @@ public class PlayerController : MonoBehaviour
     private void LateUpdate()
     {
         mainCam.transform.position = Vector3.Lerp(mainCam.transform.position, new Vector3(transform.position.x, transform.position.y, -10), Time.deltaTime * 6);
+
+        // Do the fuel thing here
     }
 
     void RotateSelf()
@@ -38,14 +44,27 @@ public class PlayerController : MonoBehaviour
         float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
 
         Quaternion rot = Quaternion.Euler(new Vector3(0, 0, angle));
-        transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime*rotateSpeed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime * rotateSpeed);
     }
 
+    /// <summary>
+    /// Moves rocket in the direction it's facing.
+    /// </summary>
     void MoveTowardsTarget()
     {
         transform.position += transform.right * Time.deltaTime * moveSpeed * SpeedMultiplier();
     }
 
+    /// <summary>
+    /// A multiplier based on cursor distance from rocket. Applies an x2 boost if isBoosting
+    /// </summary>
+    /// <returns></returns>
+    float SpeedMultiplier()
+    {
+        Debug.Log(Vector2.Distance(MouseTargetPos(), transform.position) / 2);
+        return !isBoosting ? Mathf.Clamp(Vector2.Distance(MouseTargetPos(), transform.position) / 2, 0, 1) :
+                            Mathf.Clamp(Vector2.Distance(MouseTargetPos(), transform.position), 0.5f, 2);
+    }
     Vector2 MouseTargetPos()
     {
         return mainCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
@@ -54,15 +73,5 @@ public class PlayerController : MonoBehaviour
     Vector2 TargetDir()
     {
         return MouseTargetPos() - (Vector2)transform.position;
-    }
-
-    /// <summary>
-    /// Based on mouse world distance from transform position
-    /// </summary>
-    /// <returns></returns>
-    float SpeedMultiplier()
-    {
-        Debug.Log(Vector2.Distance(MouseTargetPos(), transform.position)/2);
-        return Mathf.Clamp(Vector2.Distance(MouseTargetPos(), transform.position)/2, isBoosting?0.5f:0, isBoosting?2:1);
     }
 }
