62a485e8fe828f9a616e4d5ae9dea64ff9c9a708
IronExcavater <83278134+IronExcavater@users.noreply.github.com>
2025-06-23 15:40:46 +1000

    added animation manager.

---
+++ Assets/Scripts/Animation/AnimatorCache.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Animation
+{
+    [RequireComponent(typeof(Animator))]
+
+    public class AnimatorCache : MonoBehaviour
+    {
+        private Dictionary<string, int> _hashes;
+
+        private void Awake()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            if (_hashes != null) return;
+
+            _hashes = new Dictionary<string, int>();
+            var ani = GetComponent<Animator>();
+            foreach (var parameter in ani.parameters)
+                _hashes.Add(parameter.name, parameter.nameHash);
+        }
+
+        public int GetHash(string parameterName)
+        {
+            Initialize();
+            return _hashes.TryGetValue(parameterName, out var hash)
+                ? hash
+                : throw new KeyNotFoundException(parameterName);
+        }
+    }
+}
