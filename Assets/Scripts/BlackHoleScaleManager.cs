2ee094630fc2e622de35fed1ba77cac32dbbdc12
Alexis986 <alexplayer153@gmail.com>
2025-06-23 22:41:46 +1000

    (dev) Modified BlackHoleScaleManager

--- Assets/Scripts/BlackHoleScaleManager.cs
+++ Assets/Scripts/BlackHoleScaleManager.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class BlackHoleScaleManager : MonoBehaviour
 {
-    public Vector3 blackHoleScale;
+    [Header("Black Hole Scale Settings")]
+    public float currentScale = 1f;
+    private Vector3 blackHoleScale;
     public float smoothTime;
     private Vector3 _velocity;
 
     // Update is called once per frame
     void Update()
     {
+        blackHoleScale = new Vector3(currentScale, currentScale, currentScale);
         transform.localScale = Vector3.SmoothDamp(transform.localScale, blackHoleScale, ref _velocity, smoothTime);
     }
 }
