b62d43ba8eab0443394dc67f291e055edea588b6
Mellatrix <89572289+Mellatrix@users.noreply.github.com>
2025-06-24 01:48:14 +1000

    debugged

--- Assets/Input/PlayerController.cs
+++ Assets/Input/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
           moveSpeed = 10f,
           maxFuel = 100f,
           fuelDrainRate = 10f;
+
     [SerializeField] private RectTransform fuelGauge;
     [SerializeField] private Image fuelFillImage;
 
@@ -27,38 +28,12 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        RotateSelf();
-        MoveTowardsTarget();
-
         if (Input.GetMouseButton(0) && currentFuel > 0f)
         {
             isBoosting = true;
         }
         else
             isBoosting = false;
-    }
-
-    private void LateUpdate()
-    {
-        mainCam.transform.position = Vector3.Lerp(mainCam.transform.position, new Vector3(transform.position.x, transform.position.y, -10), Time.deltaTime * 6);
-
-        // Do the fuel thing here
-        if (isBoosting && currentFuel > 0f)
-        {
-            float drain = SpeedMultiplier() * fuelDrainRate * Time.deltaTime;
-            currentFuel = Mathf.Max(currentFuel - drain, 0f);
-
-            if (currentFuel <= 0f)
-                isBoosting = false;
-        }
-        else
-        {
-            float drain = fuelDrainRate * Time.deltaTime;
-            currentFuel = Mathf.Max(currentFuel - drain, 0f);
-
-            if (currentFuel <= 0f)
-                isBoosting = false;
-        }
 
         if (fuelGauge != null)
         {
@@ -72,13 +47,38 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
+    {
+        RotateSelf();
+        MoveTowardsTarget();
+
+        mainCam.transform.position = Vector3.Lerp(mainCam.transform.position, new Vector3(transform.position.x, transform.position.y, -10), Time.deltaTime * 6);
+
+        if (currentFuel > 0f)
+        {
+            float drain = SpeedMultiplier() * fuelDrainRate * Time.deltaTime;
+            currentFuel = Mathf.Max(currentFuel - drain, 0f);
+
+            /*if (currentFuel <= 0f)
+                isBoosting = false;*/
+        }
+        /*else
+        {
+            float drain = fuelDrainRate * Time.deltaTime;
+            currentFuel = Mathf.Max(currentFuel - drain, 0f);
+
+            if (currentFuel <= 0f)
+                isBoosting = false;
+        }*/
+    }
+
     void RotateSelf()
     {
         Vector2 targetDir = TargetDir();
         float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
 
         Quaternion rot = Quaternion.Euler(new Vector3(0, 0, angle));
-        transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime * rotateSpeed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime * rotateSpeed + SpeedMultiplier());
     }
 
     /// <summary>
@@ -95,9 +95,11 @@ public class PlayerController : MonoBehaviour
     /// <returns></returns>
     float SpeedMultiplier()
     {
-        Debug.Log(Vector2.Distance(MouseTargetPos(), transform.position) / 2);
-        return !isBoosting ? Mathf.Clamp(Vector2.Distance(MouseTargetPos(), transform.position) / 2, 0, 1) :
-                            Mathf.Clamp(Vector2.Distance(MouseTargetPos(), transform.position), 0.5f, 2);
+        float mouseDist = Vector2.Distance(MouseTargetPos(), transform.position);
+        if (mouseDist < 0.3f)
+            mouseDist = 0;
+        return !isBoosting ? Mathf.Clamp(mouseDist / 2, 0, 1) :
+                            Mathf.Clamp(mouseDist, 0.5f, 2);
     }
     Vector2 MouseTargetPos()
     {
