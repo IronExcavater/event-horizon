3fa2c10589a37229f3ba3dc8d9e999cc26d97dc0
RadioHead <qhdxbqm123@gmail.com>
2025-06-24 12:18:19 +1000

    blackhole audio added

--- Assets/Scripts/BlackHoleScaleManager.cs
+++ Assets/Scripts/BlackHoleScaleManager.cs
@@ -7,11 +7,18 @@ public class BlackHoleScaleManager : MonoBehaviour
     private Vector3 blackHoleScale;
     public float smoothTime;
     private Vector3 _velocity;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         blackHoleScale = new Vector3(currentScale, currentScale, currentScale);
         transform.localScale = Vector3.SmoothDamp(transform.localScale, blackHoleScale, ref _velocity, smoothTime);
+        audioSource.volume = Mathf.Clamp01(currentScale/15);
     }
 }
